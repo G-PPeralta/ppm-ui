5d0a17f0d5bf336184accd64ab307983fb2c0c13
ialexanderbrito <xanderzinho26@gmail.com>
2022-06-14 10:22:06 -0300

    fix: update icons sidebar

--- src/components/SidebarContent/items.ts
+++ src/components/SidebarContent/items.ts
@@ -2,9 +2,11 @@ import { IconType } from 'react-icons';
 import {
   FiHome,
   FiTrendingUp,
-  FiCompass,
-  FiStar,
+  FiEdit2,
   FiSettings,
+  FiBarChart,
+  FiUsers,
+  FiFile,
 } from 'react-icons/fi';
 
 interface LinkItemProps {
@@ -13,9 +15,11 @@ interface LinkItemProps {
   link?: string;
 }
 export const LinkItems: Array<LinkItemProps> = [
-  { name: 'Inicio', icon: FiHome, link: '/admin' },
-  { name: 'Cadastro Projetos', icon: FiTrendingUp, link: '/trending' },
-  { name: 'Cadastro de Ações', icon: FiCompass, link: '/explorer' },
-  { name: 'Relatórios', icon: FiStar, link: '/favorites' },
-  { name: 'Configurações', icon: FiSettings, link: '/configurate' },
+  { name: 'Inicio', icon: FiHome, link: '/' },
+  { name: 'Cadastro Projetos', icon: FiEdit2, link: '/projects-registration' },
+  { name: 'Cadastro de Ações', icon: FiTrendingUp, link: '/share-register' },
+  { name: 'Relatórios', icon: FiFile, link: '/reports' },
+  { name: 'Infográficos', icon: FiBarChart, link: '/infographics' },
+  { name: 'Alterar Permissões', icon: FiUsers, link: '/permissions' },
+  { name: 'Configurações', icon: FiSettings, link: '/settings' },
 ];
