4dc8cbd4d3e9df33c527f4acf1049a6b9aec1f88
ialexanderbrito <xanderzinho26@gmail.com>
2022-06-20 19:10:50 -0300

    feat: add update profile admin

--- src/hooks/usePermissions.tsx
+++ src/hooks/usePermissions.tsx
@@ -1,15 +1,22 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 
 import { useFormik } from 'formik';
-import { registerSchema } from 'validations/Register';
+import { updateProfileSchema } from 'validations/UpdateProfile';
 
 import { useToast } from 'contexts/Toast';
 
+import { getUserPending } from 'services/get/User';
+import { putProfile } from 'services/update/Profile';
+
 export function usePermissions() {
   const navigate = useNavigate();
+  const params = useParams();
+  const idUser = params.id || '0';
   const { toast } = useToast();
   const [loading, setLoading] = useState(false);
+
+  const [permission, setPermission] = useState<any>();
 
   const permissionsForm = useFormik({
     initialValues: {
@@ -20,23 +27,53 @@ export function usePermissions() {
       accessLevel: '',
       avatar: '',
     },
-    validationSchema: registerSchema,
+    validationSchema: updateProfileSchema,
     onSubmit: async (values) => {
       setLoading(true);
-      if (values) {
-        console.log(values);
-        toast.success('Cadastro realizado com sucesso!');
-        setLoading(false);
-        navigate('/');
+      const newValues = {
+        nome: values.name,
+        telefone: values.telephone,
+        email: values.email,
+        area_atuacao: values.area,
+        perfil: 'USER',
+        avatar: values.avatar,
+      };
+
+      const { status } = await putProfile(idUser, newValues);
+      if (status === 200) {
+        toast.success('Cadastro atualizado com sucesso!');
+        navigate('/permissions');
       } else {
-        console.log('Não tem valores');
+        toast.error('Erro ao atualizar cadastro!');
       }
+
       setLoading(false);
     },
   });
 
+  useEffect(() => {
+    async function findIdPermission(id: string) {
+      setLoading(true);
+      const { data, status } = await getUserPending(id);
+
+      if (status === 200) {
+        setPermission(data);
+
+        permissionsForm.setFieldValue('name', data?.nome);
+        permissionsForm.setFieldValue('telephone', data?.telefone);
+        permissionsForm.setFieldValue('email', data?.email);
+        permissionsForm.setFieldValue('area', data?.area_atuacao);
+      }
+
+      setLoading(false);
+    }
+
+    findIdPermission(idUser as string);
+  }, [idUser]);
+
   return {
     permissionsForm,
     loading,
+    permission,
   };
 }
