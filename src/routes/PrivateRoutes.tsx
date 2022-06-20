db7dbc3ef305ca916f681ea8d6a7ae4e76d4ece0
ialexanderbrito <xanderzinho26@gmail.com>
2022-06-20 18:36:20 -0300

    feat: add new routes permissions

--- src/routes/PrivateRoutes.tsx
+++ src/routes/PrivateRoutes.tsx
@@ -4,6 +4,7 @@ import { Home } from 'pages/Home';
 import { Infographics } from 'pages/Infographics';
 import { NotFound } from 'pages/NotFound';
 import { Permissions } from 'pages/Permissions';
+import { PermissionsList } from 'pages/PermissionsList';
 import { Profile } from 'pages/Profile';
 import { ProjectsRegistration } from 'pages/ProjectsRegistration';
 import { Reports } from 'pages/Reports';
@@ -18,7 +19,8 @@ export function PrivateRoutes() {
       <Route path="/share-register" element={<ShareRegister />} />
       <Route path="/reports" element={<Reports />} />
       <Route path="/infographics" element={<Infographics />} />
-      <Route path="/permissions" element={<Permissions />} />
+      <Route path="/permissions" element={<PermissionsList />} />
+      <Route path="permissions/:id" element={<Permissions />} />
       <Route path="/settings" element={<Settings />} />
       <Route path="/profile" element={<Profile />} />
       <Route path="*" element={<NotFound />} />
