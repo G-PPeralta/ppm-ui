1522f0555780411bf4fa56076992d714dc0721df
ialexanderbrito <xanderzinho26@gmail.com>
2022-06-22 19:56:13 -0300

    fix: validations trycatch

--- src/hooks/usePermissions.tsx
+++ src/hooks/usePermissions.tsx
@@ -2,10 +2,12 @@ import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 
 import { useFormik } from 'formik';
+import { ResponsePermissions, ResponseRoles } from 'interfaces/Services';
 import { updateProfileSchema } from 'validations/UpdateProfile';
 
 import { useToast } from 'contexts/Toast';
 
+import { getRoles } from 'services/get/Roles';
 import { getUserPending } from 'services/get/User';
 import { putProfile } from 'services/update/Profile';
 
@@ -16,7 +18,8 @@ export function usePermissions() {
   const { toast } = useToast();
   const [loading, setLoading] = useState(false);
 
-  const [permission, setPermission] = useState<any>();
+  const [permission, setPermission] = useState<ResponsePermissions>();
+  const [roles, setRoles] = useState<ResponseRoles[]>();
 
   const permissionsForm = useFormik({
     initialValues: {
@@ -35,38 +38,64 @@ export function usePermissions() {
         telefone: values.telephone,
         email: values.email,
         area_atuacao: values.area,
-        role_id: 2,
+        role_id: values.accessLevel,
         avatar: values.avatar,
       };
 
-      const { status } = await putProfile(idUser, newValues);
-      if (status === 200) {
-        toast.success('Cadastro atualizado com sucesso!');
-        navigate('/permissions');
-      } else {
-        toast.error('Erro ao atualizar cadastro!');
-      }
+      try {
+        const { status } = await putProfile(idUser, newValues);
+        if (status === 200) {
+          toast.success('Cadastro atualizado com sucesso!');
+          navigate('/permissions');
+        }
 
-      setLoading(false);
+        setLoading(false);
+      } catch (error) {
+        toast.error('Erro ao atualizar cadastro!');
+        setLoading(false);
+      }
     },
   });
 
   useEffect(() => {
-    async function findIdPermission(id: string) {
-      setLoading(true);
-      const { data, status } = await getUserPending(id);
+    async function findRoles() {
+      try {
+        setLoading(true);
+        const { data, status } = await getRoles();
 
-      if (status === 200) {
-        setPermission(data);
-
-        permissionsForm.setFieldValue('name', data?.nome);
-        permissionsForm.setFieldValue('telephone', data?.telefone);
-        permissionsForm.setFieldValue('email', data?.email);
-        permissionsForm.setFieldValue('area', data?.area_atuacao);
-        permissionsForm.setFieldValue('avatar', data?.avatar);
+        if (status === 200) {
+          setRoles(data);
+        }
+        setLoading(false);
+      } catch (error) {
+        toast.error('Erro ao carregar dados de acesso!');
+        setLoading(false);
       }
+    }
+    findRoles();
+  }, []);
 
-      setLoading(false);
+  useEffect(() => {
+    async function findIdPermission(id: string) {
+      try {
+        setLoading(true);
+        const { data, status } = await getUserPending(id);
+
+        if (status === 200) {
+          setPermission(data[0]);
+
+          permissionsForm.setFieldValue('name', data[0]?.nome);
+          permissionsForm.setFieldValue('telephone', data[0]?.telefone);
+          permissionsForm.setFieldValue('email', data[0]?.email);
+          permissionsForm.setFieldValue('area', data[0]?.area_atuacao);
+          permissionsForm.setFieldValue('accessLevel', data[0]?.role_id);
+        }
+
+        setLoading(false);
+      } catch (error) {
+        toast.error('Erro ao carregar cadastro!');
+        setLoading(false);
+      }
     }
 
     findIdPermission(idUser as string);
@@ -76,5 +105,6 @@ export function usePermissions() {
     permissionsForm,
     loading,
     permission,
+    roles,
   };
 }
