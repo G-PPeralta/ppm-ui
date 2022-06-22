5b758e282cdc7881a6867d2db7f8176a685af4c5
ialexanderbrito <xanderzinho26@gmail.com>
2022-06-22 20:18:43 -0300

    ci: env config

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const urlAPI = `${process.env.REACT_APP_API_URL}:${process.env.REACT_APP_API_PORT}`;
+const urlAPI = `${process.env.REACT_APP_API_URL}`;
 
 export const api = axios.create({
   baseURL: urlAPI,
