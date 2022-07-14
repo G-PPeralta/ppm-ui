ec1d5cb589bf871327a8d985a07e97c094a8fce6
Eduardo Muchak <eduardomuchak@gmail.com>
2022-07-14 13:36:26 -0300

    feat: add scurve chart component file still not working 100%

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,13 @@
 import { Gantt } from 'components/Gantt';
+import SCurveChart from 'components/SCurveChart';
 import Sidebar from 'components/SideBar';
 
 export function Home() {
   return (
     <>
       <Sidebar>
-        <Gantt></Gantt>
+        <Gantt />
+        <SCurveChart />
       </Sidebar>
     </>
   );
