618a748b6190b7adca46e7ff0f8947d0b0e5713c
Eduardo Muchak <eduardomuchak@gmail.com>
2022-08-17 12:17:58 -0300

    fix: :art: ajusta loading do component gantt
    temporariamente ate integracao com backend

--- src/components/Gantt/index.tsx
+++ src/components/Gantt/index.tsx
@@ -64,7 +64,9 @@ export function Gantt() {
   }
 
   useEffect(() => {
-    setLoading(false);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
   }, [gantt]);
 
   useEffect(() => {
--- src/pages/DetalhamentoProjeto/index.tsx
+++ src/pages/DetalhamentoProjeto/index.tsx
@@ -5,7 +5,7 @@ import { Box, Flex } from '@chakra-ui/react';
 import { Ring } from '@uiball/loaders';
 import { ICardInfoProjeto } from 'interfaces/DetalhamentoProjetos';
 
-// import { Gantt } from 'components/Gantt';
+import { Gantt } from 'components/Gantt';
 import Sidebar from 'components/SideBar';
 
 import { getInfoProjetos } from 'services/get/DetalhamentoProjetos';
@@ -46,7 +46,12 @@ function DetalhamentoProjeto() {
     <>
       <Sidebar>
         {loading ? (
-          <Box display={'flex'} alignItems={'center'} justifyContent={'center'}>
+          <Box
+            display={'flex'}
+            alignItems={'center'}
+            justifyContent={'center'}
+            h={'84vh'}
+          >
             <Ring speed={2} lineWeight={5} color="blue" size={64} />
           </Box>
         ) : (
@@ -64,7 +69,7 @@ function DetalhamentoProjeto() {
               <CardOrcamento />
               <BotoesModais />
             </Flex>
-            {/* <Gantt /> */}
+            <Gantt />
             <GraficoCurvaS />
           </>
         )}
