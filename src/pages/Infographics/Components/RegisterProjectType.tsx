01a5867759ee10e01f26a450eadb5fd3d9936c43
vinnidias <vinicius.3991@gmail.com>
2022-08-18 11:03:40 -0300

    chore: initial structure of component

---
+++ src/pages/Infographics/Components/RegisterProjectType.tsx
@@ -0,0 +1,43 @@
+// import React from "react";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+} from '@chakra-ui/react';
+
+type Props = {
+  onOpen: any;
+  onClose: any;
+  isOpen: boolean;
+};
+
+export function RegisterProjectType({ onOpen, isOpen, onClose }: Props) {
+  return (
+    <>
+      <Button onClick={onOpen}>Open Modal</Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <p>Nome </p>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant="ghost">Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
