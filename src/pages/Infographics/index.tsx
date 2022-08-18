b1b3c1d4b34f4ad3d38f37ba1a977e6ed659ae24
vinnidias <vinicius.3991@gmail.com>
2022-08-18 11:30:10 -0300

    chore: functional adjust

--- src/pages/Infographics/index.tsx
+++ src/pages/Infographics/index.tsx
@@ -1,10 +1,23 @@
+import { useState } from 'react';
+
 import Sidebar from 'components/SideBar';
 
+import { RegisterProjectType } from './Components/RegisterProjectType';
+
 export function Infographics() {
+  const [modalIsVisible, setModalIsVisible] = useState(false);
+
   return (
     <>
       <Sidebar>
         <h1>Infographics</h1>
+        <button onClick={() => setModalIsVisible(true)}>Modal</button>
+
+        <RegisterProjectType
+          onClose={() => setModalIsVisible(false)}
+          onOpen={() => setModalIsVisible(false)}
+          isOpen={modalIsVisible}
+        />
       </Sidebar>
     </>
   );
