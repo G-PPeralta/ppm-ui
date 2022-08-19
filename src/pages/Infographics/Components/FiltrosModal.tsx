6fa1cd1b8e975d00f5568e9ef7caac045d9a81a2
Eduardo Muchak <eduardomuchak@gmail.com>
2022-08-19 12:26:06 -0300

    feat: :sparkles: modal de filtragem finalizado
    nao esta funcional. apenas layout estatico

--- src/pages/Infographics/Components/FiltrosModal.tsx
+++ src/pages/Infographics/Components/FiltrosModal.tsx
@@ -1,3 +1,5 @@
+import { IoMdArrowDropdown } from 'react-icons/io';
+
 import {
   Button,
   Flex,
@@ -12,6 +14,9 @@ import {
   ModalHeader,
   ModalOverlay,
   useDisclosure,
+  Box,
+  Input,
+  useBreakpointValue,
 } from '@chakra-ui/react';
 
 function FiltrosModal() {
@@ -19,12 +24,17 @@ function FiltrosModal() {
 
   return (
     <>
-      <Button onClick={onOpen}>Open Modal</Button>
+      <Button
+        onClick={onOpen}
+        rightIcon={<IoMdArrowDropdown />}
+        colorScheme="blue"
+        variant="ghost"
+      >
+        Filtrar
+      </Button>
       <Modal isOpen={isOpen} onClose={onClose} size={'2xl'}>
         <ModalOverlay />
-        <ModalContent
-        // width="70%" height={580} borderRadius={8} marginTop={3}
-        >
+        <ModalContent>
           <ModalHeader
             backgroundColor={'#2E69FD'}
             borderTopRadius={7}
@@ -36,28 +46,89 @@ function FiltrosModal() {
             Filtros
           </ModalHeader>
           <ModalCloseButton color={'white'} />
-          <ModalBody>
+          <ModalBody mt={4}>
             <FormControl>
-              <FormLabel htmlFor="area">Área</FormLabel>
-              <Select variant="unstyled">
-                <option value="1">Valor 1</option>
-                <option value="2">Valor 2</option>
-                <option value="3">Valor 3</option>
-              </Select>
+              <Flex direction={'column'} gap={5}>
+                <Box>
+                  <FormLabel htmlFor="area">Área</FormLabel>
+                  <Select variant="unstyled">
+                    <option value="1">Valor 1</option>
+                    <option value="2">Valor 2</option>
+                    <option value="3">Valor 3</option>
+                  </Select>
+                </Box>
 
-              <FormLabel htmlFor="poco">Poço</FormLabel>
-              <Select variant="unstyled">
-                <option value="1">Valor 1</option>
-                <option value="2">Valor 2</option>
-                <option value="3">Valor 3</option>
-              </Select>
+                <Box>
+                  <FormLabel htmlFor="poco">Poço</FormLabel>
+                  <Select variant="unstyled">
+                    <option value="1">Valor 1</option>
+                    <option value="2">Valor 2</option>
+                    <option value="3">Valor 3</option>
+                  </Select>
+                </Box>
 
-              <FormLabel htmlFor="atividade">Atividade</FormLabel>
-              <Select variant="unstyled">
-                <option value="1">Valor 1</option>
-                <option value="2">Valor 2</option>
-                <option value="3">Valor 3</option>
-              </Select>
+                <Box>
+                  <FormLabel htmlFor="atividade">Atividade</FormLabel>
+                  <Select variant="unstyled">
+                    <option value="1">Valor 1</option>
+                    <option value="2">Valor 2</option>
+                    <option value="3">Valor 3</option>
+                  </Select>
+                </Box>
+
+                <Box>
+                  <FormLabel htmlFor="responsavel">Responsável</FormLabel>
+                  <Select variant="unstyled">
+                    <option value="1">Valor 1</option>
+                    <option value="2">Valor 2</option>
+                    <option value="3">Valor 3</option>
+                  </Select>
+                </Box>
+
+                <Flex justify={'space-between'} gap={5}>
+                  <Flex direction={'column'} grow={1}>
+                    <FormLabel htmlFor="dataInicio">Data Inicio</FormLabel>
+                    <Input
+                      isRequired
+                      placeholder="dd/mm/aaaa"
+                      id="dataInicio"
+                      type="date"
+                      name="dataInicio"
+                      w={useBreakpointValue({ base: '100%', md: '100%' })}
+                    />
+                  </Flex>
+
+                  <Flex direction={'column'} grow={1}>
+                    <FormLabel htmlFor="dataFim">Data Fim</FormLabel>
+                    <Input
+                      isRequired
+                      placeholder="dd/mm/aaaa"
+                      id="dataFim"
+                      type="date"
+                      name="dataFim"
+                      w={useBreakpointValue({ base: '100%', md: '100%' })}
+                    />
+                  </Flex>
+                </Flex>
+
+                <Box>
+                  <FormLabel htmlFor="sonda">Sonda</FormLabel>
+                  <Select variant="unstyled">
+                    <option value="1">Valor 1</option>
+                    <option value="2">Valor 2</option>
+                    <option value="3">Valor 3</option>
+                  </Select>
+                </Box>
+
+                <Box>
+                  <FormLabel htmlFor="status">Status</FormLabel>
+                  <Select variant="unstyled">
+                    <option value="1">Valor 1</option>
+                    <option value="2">Valor 2</option>
+                    <option value="3">Valor 3</option>
+                  </Select>
+                </Box>
+              </Flex>
             </FormControl>
           </ModalBody>
           <ModalFooter display={'flex'} justifyContent={'center'}>
