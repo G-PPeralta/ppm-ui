03c2cf9a74046055343d121055fab6e5f0b98f12
Eduardo Muchak <eduardomuchak@gmail.com>
2022-08-31 10:44:41 -0300

    feat: :sparkles: adiciona rota generica para enviar dadso cadastro intervencao para backend

--- src/pages/Infographics/Components/ModalBotaoCadastrar.tsx
+++ src/pages/Infographics/Components/ModalBotaoCadastrar.tsx
@@ -12,10 +12,22 @@ import {
   ModalBody,
   ModalFooter,
   useDisclosure,
+  Button,
 } from '@chakra-ui/react';
+import { Ring } from '@uiball/loaders';
+
+import { useCadastroIntervencao } from 'hooks/useCadastroIntervencao';
+
+import { postIntervencao } from 'services/post/CadastroIntervencao';
 
 function ModalBotaoCadastrar() {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const { intervencaoForm, loading } = useCadastroIntervencao();
+
+  const handleCadastrar = () => {
+    postIntervencao(intervencaoForm.values);
+    onClose();
+  };
 
   return (
     <>
@@ -66,24 +78,50 @@ function ModalBotaoCadastrar() {
             Cadastrar Nova Intervenção/Perfuração
           </ModalHeader>
           <ModalCloseButton color={'white'} />
-          <ModalBody></ModalBody>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              intervencaoForm.handleSubmit(e);
+            }}
+          >
+            <ModalBody></ModalBody>
 
-          <ModalFooter>
-            {/* <Center justifySelf="center" width="100%">
-            <Button variant="ghost" color="red">
-              Cancelar
-            </Button>
-            <Button
-              colorScheme="blue"
-              backgroundColor="#0047BB"
-              mr={3}
-              onClick={() => console.log('submeteu')}
-              color="white"
-            >
-              Concluir cadastro
-            </Button>
-          </Center> */}
-          </ModalFooter>
+            <ModalFooter>
+              <Flex gap={2} align={'center'} justify={'center'}>
+                <Button
+                  variant="ghost"
+                  color="red"
+                  onClick={() => onClose()}
+                  _hover={{
+                    background: 'red.500',
+                    transition: 'all 0.4s',
+                    color: 'white',
+                  }}
+                >
+                  Cancelar
+                </Button>
+                <Button
+                  disabled={!intervencaoForm.isValid}
+                  background="origem.300"
+                  variant="primary"
+                  color="white"
+                  onClick={() => handleCadastrar()}
+                  _hover={{
+                    background: 'origem.500',
+                    transition: 'all 0.4s',
+                  }}
+                >
+                  {loading ? (
+                    <Ring speed={2} lineWeight={5} color="white" size={24} />
+                  ) : (
+                    <>
+                      <Text>Concluir Cadastro</Text>
+                    </>
+                  )}
+                </Button>
+              </Flex>
+            </ModalFooter>
+          </form>
         </ModalContent>
       </Modal>
     </>
--- src/services/get/Infograficos.tsx
+++ src/services/get/Infograficos.tsx
@@ -4,7 +4,7 @@ export async function getInfoCampanha(): Promise<{
   data: any;
   status: number;
 }> {
-  const { data, status } = await api.get(`/campanha/`, {
+  const { data, status } = await api.get(`/campanha`, {
     headers: {
       Authorization: `Bearer ${sessionStorage.getItem('@Origem:token')}`,
     },
