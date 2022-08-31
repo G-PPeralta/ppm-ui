30c2f4a0f51a64c6592ce84a69aecb0d5c6d7624
Eduardo Muchak <eduardomuchak@gmail.com>
2022-08-31 11:58:41 -0300

    feat: :sparkles: adiciona logica de loading tela infograficos
    melhoria experiencia usuario enquanto frontend faz requisicao ao backend

--- src/pages/Infographics/Components/ColumnSPT.tsx
+++ src/pages/Infographics/Components/ColumnSPT.tsx
@@ -17,55 +17,6 @@ type Props = {
 };
 
 function ColumnSPT({ column }: Props) {
-  // console.log('COLUNA', column);
-  // const infoCards = [
-  //   {
-  //     nome: 'PIR 001',
-  //     data: '12/04/2022',
-  //     porcentagem: `${Math.floor(Math.random() * 100)}%`,
-  //   },
-  //   {
-  //     nome: 'PIR 002',
-  //     data: '12/04/2022',
-  //     porcentagem: `${Math.floor(Math.random() * 100)}%`,
-  //   },
-  //   {
-  //     nome: 'PIR 003',
-  //     data: '12/04/2022',
-  //     porcentagem: `${Math.floor(Math.random() * 100)}%`,
-  //   },
-  //   {
-  //     nome: 'PIR 004',
-  //     data: '12/04/2022',
-  //     porcentagem: `${Math.floor(Math.random() * 100)}%`,
-  //   },
-  //   {
-  //     nome: 'PIR 005',
-  //     data: '12/04/2022',
-  //     porcentagem: `${Math.floor(Math.random() * 100)}%`,
-  //   },
-  //   {
-  //     nome: 'PIR 006',
-  //     data: '12/04/2022',
-  //     porcentagem: `${Math.floor(Math.random() * 100)}%`,
-  //   },
-  //   {
-  //     nome: 'PIR 007',
-  //     data: '12/04/2022',
-  //     porcentagem: `${Math.floor(Math.random() * 100)}%`,
-  //   },
-  //   {
-  //     nome: 'PIR 008',
-  //     data: '12/04/2022',
-  //     porcentagem: `${Math.floor(Math.random() * 100)}%`,
-  //   },
-  //   // {
-  //   //   nome: 'PIR 009',
-  //   //   data: '12/04/2022',
-  //   //   porcentagem: `${Math.floor(Math.random() * 100)}%`,
-  //   // },
-  // ];
-
   return (
     <Flex direction={'column'} align={'center'} justify={'center'}>
       <Text fontSize={'2xl'} fontWeight={'bold'} mb={6} mt={3}>
--- src/pages/Infographics/index.tsx
+++ src/pages/Infographics/index.tsx
@@ -1,16 +1,7 @@
-// import { useState } from 'react';
-
 import { useEffect, useState } from 'react';
 
-import {
-  Box,
-  Button,
-  Flex,
-  Heading,
-  Stack,
-  useBreakpointValue,
-  useColorModeValue,
-} from '@chakra-ui/react';
+import { Box, Button, Flex, Heading, Stack } from '@chakra-ui/react';
+import { Ring } from '@uiball/loaders';
 
 import Sidebar from 'components/SideBar';
 
@@ -39,9 +30,6 @@ export function Infographics() {
     handleGetCampanha();
   }, []);
 
-  console.log(loading);
-  // console.log(campanhas);
-
   const statusProjeto = [
     {
       status: 'Não Aplicável',
@@ -65,27 +53,6 @@ export function Infographics() {
     },
   ];
 
-  // const infoColunas = [
-  //   {
-  //     nome: 'SPT - 112',
-  //   },
-  //   {
-  //     nome: 'SPT - 30',
-  //   },
-  //   {
-  //     nome: 'SPT - 131',
-  //   },
-  //   {
-  //     nome: 'SPT - 108',
-  //   },
-  //   {
-  //     nome: 'SPT - 061',
-  //   },
-  //   {
-  //     nome: 'SC-XX',
-  //   },
-  // ];
-
   return (
     <>
       <Sidebar>
@@ -94,122 +61,110 @@ export function Infographics() {
           isOpen={modalIsVisible}
           onClose={() => setModalIsVisible(false)}
         /> */}
-        <Stack spacing="8">
-          <Flex
-            w={useBreakpointValue({ base: '100%', md: 'auto' })}
-            align="center"
-            justify="center"
-            bg={useBreakpointValue({ base: 'white', sm: '#EDF2F7' })}
-          >
-            <Box
-              py={{ base: '0', sm: '8' }}
-              px={{ base: '4', sm: '6' }}
-              w={useBreakpointValue({
-                base: '100%',
-                sm: '100%',
-                md: '100%',
-                lg: '100%',
-              })}
-              bg={useBreakpointValue({ base: 'transparent', sm: 'white' })}
-              boxShadow={{
-                base: 'none',
-                sm: useColorModeValue('md', 'md-dark'),
-              }}
-              borderRadius={{ base: 'none', sm: 'xl' }}
-            >
-              <Flex justify={'space-between'} mb={5}>
-                <Heading as="h3" size="md" mb={3}>
-                  Acompanhamento de poços
-                </Heading>
-                <Flex gap={4}>
-                  <ExibirModal />
-                  <FiltrosModal />
+        {!loading ? (
+          <Stack spacing="8">
+            <Flex w={'auto'} align="center" justify="center" bg={'#EDF2F7'}>
+              <Box
+                py={{ base: '0', sm: '8' }}
+                px={{ base: '4', sm: '6' }}
+                w={'100%'}
+                bg={'white'}
+                borderRadius={{ base: 'none', sm: 'xl' }}
+              >
+                <Flex justify={'space-between'} mb={5}>
+                  <Heading as="h3" size="md" mb={3}>
+                    Acompanhamento de poços
+                  </Heading>
+                  <Flex gap={4}>
+                    <ExibirModal />
+                    <FiltrosModal />
+                  </Flex>
                 </Flex>
-              </Flex>
-              <Flex justify={'space-between'} gap={6} wrap={'wrap'} mb={4}>
-                <Flex gap={2}>
-                  <Button
-                    variant="outline"
-                    border={'2px solid'}
-                    borderColor={'origem.500'}
-                    textColor={'origem.500'}
-                    _hover={{
-                      borderColor: 'origem.600',
-                      backgroundColor: 'origem.500',
-                      textColor: 'white',
-                      transition: 'all 0.4s',
-                    }}
-                  >
-                    Sonda
-                  </Button>
-                  <Button
-                    variant="outline"
-                    border={'2px solid'}
-                    borderColor={'origem.500'}
-                    textColor={'origem.500'}
-                    _hover={{
-                      borderColor: 'origem.600',
-                      backgroundColor: 'origem.500',
-                      textColor: 'white',
-                      transition: 'all 0.4s',
-                    }}
-                  >
-                    Projeto
-                  </Button>
-                  <Button
-                    variant="outline"
-                    border={'2px solid'}
-                    borderColor={'origem.500'}
-                    textColor={'origem.500'}
-                    _hover={{
-                      borderColor: 'origem.600',
-                      backgroundColor: 'origem.500',
-                      textColor: 'white',
-                      transition: 'all 0.4s',
-                    }}
-                  >
-                    Atividade
-                  </Button>
-                </Flex>
-                <Flex gap={4} wrap={'wrap'}>
-                  {statusProjeto.map((status, index) => (
-                    <StatusProjeto
-                      key={index}
-                      status={status.status}
-                      color={status.color}
-                    />
-                  ))}
-                </Flex>
-              </Flex>
-              <Flex align={'center'} justify={'center'}>
-                <Box
-                  overflowX={{ base: 'scroll' }}
-                  display={'flex'}
-                  flexDirection={'row'}
-                  gap={10}
-                  py={4}
-                >
-                  {campanhas.map((column, index) => (
-                    <Flex
-                      key={index}
-                      direction={'column'}
-                      gap={4}
-                      align={'center'}
-                      justify={'center'}
+                <Flex justify={'space-between'} gap={6} wrap={'wrap'} mb={4}>
+                  <Flex gap={2}>
+                    <Button
+                      variant="outline"
+                      border={'2px solid'}
+                      borderColor={'origem.500'}
+                      textColor={'origem.500'}
+                      _hover={{
+                        borderColor: 'origem.600',
+                        backgroundColor: 'origem.500',
+                        textColor: 'white',
+                        transition: 'all 0.4s',
+                      }}
                     >
-                      <ColumnSPT column={column} />
-                      {/*
-                      <BotaoCadastrar />
-                      SISTEMA DE POPOVER FOI DESCONTINUADO. DEIXEI COMENTADO ATÉ SER CONFIRMADO ESSA INFORMAÇÃO
-                      */}
-                      <ModalBotaoCadastrar />
-                    </Flex>
-                  ))}
-                </Box>
-              </Flex>
-            </Box>
+                      Sonda
+                    </Button>
+                    <Button
+                      variant="outline"
+                      border={'2px solid'}
+                      borderColor={'origem.500'}
+                      textColor={'origem.500'}
+                      _hover={{
+                        borderColor: 'origem.600',
+                        backgroundColor: 'origem.500',
+                        textColor: 'white',
+                        transition: 'all 0.4s',
+                      }}
+                    >
+                      Projeto
+                    </Button>
+                    <Button
+                      variant="outline"
+                      border={'2px solid'}
+                      borderColor={'origem.500'}
+                      textColor={'origem.500'}
+                      _hover={{
+                        borderColor: 'origem.600',
+                        backgroundColor: 'origem.500',
+                        textColor: 'white',
+                        transition: 'all 0.4s',
+                      }}
+                    >
+                      Atividade
+                    </Button>
+                  </Flex>
+                  <Flex gap={4} wrap={'wrap'}>
+                    {statusProjeto.map((status, index) => (
+                      <StatusProjeto
+                        key={index}
+                        status={status.status}
+                        color={status.color}
+                      />
+                    ))}
+                  </Flex>
+                </Flex>
+                <Flex align={'center'} justify={'center'}>
+                  <Box
+                    overflowX={{ base: 'scroll' }}
+                    display={'flex'}
+                    flexDirection={'row'}
+                    gap={10}
+                    py={4}
+                  >
+                    {campanhas.map((column, index) => (
+                      <Flex
+                        key={index}
+                        direction={'column'}
+                        gap={4}
+                        align={'center'}
+                        justify={'center'}
+                      >
+                        <ColumnSPT column={column} />
+                        <ModalBotaoCadastrar />
+                      </Flex>
+                    ))}
+                  </Box>
+                </Flex>
+              </Box>
+            </Flex>
+          </Stack>
+        ) : (
+          <Flex display={'flex'} align={'center'} justify={'center'} h={'90vh'}>
+            <Ring speed={2} lineWeight={5} color="blue" size={64} />
           </Flex>
-        </Stack>
+        )}
       </Sidebar>
     </>
   );
