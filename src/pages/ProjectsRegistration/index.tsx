137e3e3f4cac7830052738f11a0d2265e1b41f5a
Bruno Alcantara <bruno.alcantara@iksolution.com.br>
2022-09-01 10:06:53 -0300

    fix: "remover capex previsto"

--- src/pages/ProjectsRegistration/index.tsx
+++ src/pages/ProjectsRegistration/index.tsx
@@ -165,7 +165,7 @@ export function ProjectsRegistration() {
                         })}
                       >
                         <FormValorTotalPrevisto projectsForm={projectsForm} />
-                        <FormValorTotalPrevisto projectsForm={projectsForm} />
+                        {/* <FormValorTotalPrevisto projectsForm={projectsForm} /> */}
                         <FormComplexidade projectsForm={projectsForm} />
                       </Flex>
                       <Flex
