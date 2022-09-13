21a14ad908e0f817de455f52de8a3e019aedcf01
Felipe Mateus <eu@felipemateus.com>
2022-09-13 12:06:12 -0300

    fix(authorizartion): fix authorization

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,3 +5,9 @@ const urlAPI = `${process.env.REACT_APP_API_URL}`;
 export const api = axios.create({
   baseURL: urlAPI,
 });
+
+export const token = {
+  headers: {
+    Authorization: `Bearer ${sessionStorage.getItem('@Origem:token')}`,
+  },
+};
--- src/services/post/CadastroModaisInfograficos.tsx
+++ src/services/post/CadastroModaisInfograficos.tsx
@@ -7,33 +7,33 @@ import {
   CadastroPoco,
 } from 'interfaces/CadastrosModaisInfograficos';
 
-import { api } from 'services/api';
+import { api, token } from 'services/api';
 
 export async function postCadastroTarefa(
   payload: CadastroTarefa,
 ): Promise<{ status: number }> {
-  const { status } = await api.post('/tarefa', payload);
+  const { status } = await api.post('/tarefa', payload, token);
   return { status };
 }
 
 export async function postCadastroSonda(
   payload: CadastroSonda,
 ): Promise<{ status: number }> {
-  const { status } = await api.post('/sonda', payload);
+  const { status } = await api.post('/sonda', payload, token);
   return { status };
 }
 
 export async function postCadastroIntervencao(
   payload: CadastroIntervencao,
 ): Promise<{ status: number }> {
-  const { status } = await api.post('/intervencoes', payload);
+  const { status } = await api.post('/intervencoes', payload, token);
   return { status };
 }
 
 export async function postCadastroAtividade(
   payload: CadastroAtividade,
 ): Promise<{ status: number }> {
-  const { status } = await api.post('/atividades-intervencoes', payload);
+  const { status } = await api.post('/atividades-intervencoes', payload, token);
   return { status };
 }
 
