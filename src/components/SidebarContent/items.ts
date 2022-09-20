ce6532c901ebdc2385d17ab75449255c16aab3f6
Bruno Alcantara <bruno.alcantara@iksolution.com.br>
2022-09-20 17:28:07 -0300

    feat: "cascata no sidebar"

--- src/components/SidebarContent/items.ts
+++ src/components/SidebarContent/items.ts
@@ -20,56 +20,55 @@ interface LinkItemProps {
   name: string;
   icon: IconType;
   link?: string;
+  children: any[];
 }
 export const LinkItems: Array<LinkItemProps> = [
-  { name: "Carteira de Projetos", icon: FiHome, link: "/" },
-  // { name: 'Gantt', icon: BsBarChartSteps, link: '/gantt' },
-  // { name: "Cadastro Projetos", icon: FiEdit2, link: "/projects-registration" },
-  // {
-  //   name: "Cadastro Fornecedores",
-  //   icon: FiEdit2,
-  //   link: "/providers-registration",
-  // },
-  { name: "Dashboard", icon: FiHome, link: "/" },
-  { name: "Projetos", icon: FaRegMoneyBillAlt, link: "/projects" },
-  { name: "Fornecedores", icon: FaRegMoneyBillAlt, link: "/" },
-  { name: "Financeiro", icon: FaRegMoneyBillAlt, link: "/" },
-  { name: "Importar Dados", icon: BiData, link: "/" },
-  // {
-  //   name: 'Cadastro de Ações',
-  //   icon: FiTrendingUp,
-  //   link: '/activities-registration',
-  // },
+  {
+    name: "Carteira de Projetos",
+    icon: FiHome,
+    children: [
+      { name: "Dashboard", icon: FiHome, link: "/" },
+      { name: "Projetos", icon: FaRegMoneyBillAlt, link: "/projects" },
+      { name: "Fornecedores", icon: FaRegMoneyBillAlt, link: "/" },
+      { name: "Financeiro", icon: FaRegMoneyBillAlt, link: "/" },
+      { name: "Importar Dados", icon: BiData, link: "/" },
+    ],
+  },
 
-  { name: "Infográficos", icon: FiBarChart, link: "/infographics" },
+  {
+    name: "Infográficos",
+    icon: FiBarChart,
+    children: [
+      { name: "Infográficos", icon: FiBarChart, link: "/infographics" },
+    ],
+  },
 
-  { name: "Estatísticas", icon: AiOutlineBarChart, link: "/" },
-  { name: "Gráficos Estatisticos", icon: AiOutlineBarChart, link: "/" },
-  { name: "Look Ahead", icon: AiOutlineBarChart, link: "/" },
-  { name: "Relatórios", icon: FiFile, link: "/reports" },
-  // { name: 'Alterar Permissões', icon: FiUsers, link: '/permissions' },
+  {
+    name: "Estatísticas",
+    icon: AiOutlineBarChart,
+    children: [
+      { name: "Estatísticas", icon: AiOutlineBarChart, link: "/" },
+      { name: "Gráficos Estatisticos", icon: AiOutlineBarChart, link: "/" },
+      { name: "Look Ahead", icon: AiOutlineBarChart, link: "/" },
+      { name: "Relatórios", icon: FiFile, link: "/reports" },
+    ],
+  },
 
-  { name: "Lixeira", icon: FiTrash2, link: "/" },
+  {
+    name: "Lixeira",
+    icon: FiTrash2,
+    children: [{ name: "Lixeira", icon: FiTrash2, link: "/" }],
+  },
 
-  { name: "Configurações", icon: FiSettings, link: "/" },
-  { name: "Usuários", icon: FiSettings, link: "/" },
-  { name: "Perfil", icon: FiSettings, link: "/" },
-  { name: "Gestão de Áreas", icon: FiSettings, link: "/" },
-  { name: "Configurar Prioridades", icon: FiSettings, link: "/" },
-
-  // {
-  //   name: "Fornecedores",
-  //   icon: FiActivity,
-  //   link: "/fornecedores",
-  // },
-  // {
-  //   name: "Detalhamento Projeto",
-  //   icon: BsBarChartSteps,
-  //   link: "/detalhamento/191",
-  // },
-  // {
-  //   name: "Projetos",
-  //   icon: FiGrid,
-  //   link: "/projects/",
-  // },
+  {
+    name: "Configurações",
+    icon: FiSettings,
+    children: [
+      { name: "Configurações", icon: FiSettings, link: "/" },
+      { name: "Usuários", icon: FiSettings, link: "/" },
+      { name: "Perfil", icon: FiSettings, link: "/" },
+      { name: "Gestão de Áreas", icon: FiSettings, link: "/" },
+      { name: "Configurar Prioridades", icon: FiSettings, link: "/" },
+    ],
+  },
 ];
