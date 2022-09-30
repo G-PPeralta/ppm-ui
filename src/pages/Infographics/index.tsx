89f6cbb01061656cbe3b815bffb1e53232993d1b
Eduardo Muchak <eduardomuchak@gmail.com>
2022-09-30 11:08:43 -0300

    style: :art: ajuste layout tela principal infograficos

--- src/pages/Infographics/index.tsx
+++ src/pages/Infographics/index.tsx
@@ -1,12 +1,6 @@
 import { useEffect, useState } from "react";
 
-import {
-  Box,
-  Flex,
-  Heading,
-  Stack,
-  useBreakpointValue,
-} from "@chakra-ui/react";
+import { Box, Flex, Heading, useBreakpointValue } from "@chakra-ui/react";
 import { Ring } from "@uiball/loaders";
 
 import Sidebar from "components/SideBar";
@@ -18,7 +12,6 @@ import { postGetInfoCampanha } from "services/get/Infograficos";
 
 import { statusProjeto } from "../../utils/validateDate";
 import ColumnSPT from "./Components/ColumnSPT";
-// import ExibirModal from "./Components/ExibirModal";
 import FiltrosModal from "./Components/FiltrosModal";
 import ModalCadastrarSonda from "./Components/ModalCadastrarSonda";
 import ModalCadastroAtividade from "./Components/ModalCadastroAtividade";
@@ -69,95 +62,93 @@ export function Infographics() {
     <>
       <Sidebar>
         {!loading ? (
-          <Stack spacing="8">
-            <Flex w={"auto"} align="center" justify="center" bg={"#EDF2F7"}>
-              <Box
-                py={{ base: "6", sm: "8" }}
-                px={{ base: "6", sm: "8" }}
-                w={"100%"}
-                bg={"white"}
-                borderRadius={{ base: "xl", sm: "xl" }}
+          <Flex w={"auto"} align="center" justify="center" bg={"#EDF2F7"}>
+            <Box
+              py={{ base: "6", sm: "8" }}
+              px={{ base: "6", sm: "8" }}
+              w={"100%"}
+              bg={"white"}
+              borderRadius={{ base: "xl", sm: "xl" }}
+            >
+              <Flex justify={"space-between"} mb={2} wrap={"wrap"}>
+                <Heading as="h3" size="md" mb={2} mt={innerWidth}>
+                  Acompanhamento de Poços
+                </Heading>
+                <Flex gap={4}>
+                  <FiltrosModal
+                    refresh={refresh}
+                    setRefresh={setRefresh}
+                    listas={listas}
+                    registerForm={registerForm}
+                  />
+                </Flex>
+              </Flex>
+              <Flex
+                direction={"column"}
+                justify={"space-between"}
+                gap={4}
+                wrap={"wrap"}
+                mb={2}
+                flex={1}
               >
-                <Flex justify={"space-between"} mb={5} wrap={"wrap"}>
-                  <Heading as="h3" size="md" mb={3} mt={innerWidth}>
-                    Acompanhamento de poços
-                  </Heading>
-                  <Flex gap={4}>
-                    {/* <ExibirModal opcoesExibir={opcoesExibir} /> */}
-                    <FiltrosModal
-                      refresh={refresh}
-                      setRefresh={setRefresh}
-                      listas={listas}
-                      registerForm={registerForm}
-                    />
-                  </Flex>
+                <Flex gap={2} wrap={"wrap"} flex={1}>
+                  <ModalNovaCampanha
+                    refresh={refresh}
+                    setRefresh={setRefresh}
+                  />
+                  <ModalCadastrarSonda />
+                  <ModalCadastroPoco />
+                  <ModalCadastroAtividade />
+                  <ModalCadastroProjetoTipo
+                    refresh={refresh}
+                    setRefresh={setRefresh}
+                  />
                 </Flex>
-                <Flex
-                  direction={"column"}
-                  justify={"space-between"}
-                  gap={6}
-                  wrap={"wrap"}
-                  mb={4}
-                >
-                  <Flex gap={2} wrap={"wrap"}>
-                    <ModalNovaCampanha
-                      refresh={refresh}
-                      setRefresh={setRefresh}
+                <Flex gap={4} wrap={"wrap"} flex={1} justify={"end"}>
+                  {statusProjeto.map((status, index) => (
+                    <StatusProjeto
+                      key={index}
+                      status={status.status}
+                      color={status.color}
                     />
-                    <ModalCadastrarSonda />
-                    <ModalCadastroPoco />
-                    <ModalCadastroAtividade />
-                    <ModalCadastroProjetoTipo
-                      refresh={refresh}
-                      setRefresh={setRefresh}
-                    />
-                  </Flex>
-                  <Flex gap={4} wrap={"wrap"}>
-                    {statusProjeto.map((status, index) => (
-                      <StatusProjeto
+                  ))}
+                </Flex>
+              </Flex>
+              <Flex align={"center"} justify={"center"}>
+                {campanhas.length !== 0 ? (
+                  <Box
+                    overflowX={{ base: "scroll" }}
+                    display={"flex"}
+                    flexDirection={"row"}
+                    gap={10}
+                    py={2}
+                  >
+                    {campanhas.map((column, index) => (
+                      <Flex
                         key={index}
-                        status={status.status}
-                        color={status.color}
-                      />
+                        direction={"column"}
+                        gap={4}
+                        align={"end"}
+                        justify={"space-between"}
+                      >
+                        <ColumnSPT
+                          column={column}
+                          refresh={refresh}
+                          setRefresh={setRefresh}
+                        />
+                      </Flex>
                     ))}
+                  </Box>
+                ) : (
+                  <Flex h={180} align={"center"} justify={"center"}>
+                    <Heading as="h4" size="md">
+                      Nenhuma campanha Cadastrada
+                    </Heading>
                   </Flex>
-                </Flex>
-                <Flex align={"center"} justify={"center"}>
-                  {campanhas.length !== 0 ? (
-                    <Box
-                      overflowX={{ base: "scroll" }}
-                      display={"flex"}
-                      flexDirection={"row"}
-                      gap={10}
-                      py={4}
-                    >
-                      {campanhas.map((column, index) => (
-                        <Flex
-                          key={index}
-                          direction={"column"}
-                          gap={4}
-                          align={"end"}
-                          justify={"space-between"}
-                        >
-                          <ColumnSPT
-                            column={column}
-                            refresh={refresh}
-                            setRefresh={setRefresh}
-                          />
-                        </Flex>
-                      ))}
-                    </Box>
-                  ) : (
-                    <Flex h={180} align={"center"} justify={"center"}>
-                      <Heading as="h4" size="md">
-                        Nenhuma campanha Cadastrada
-                      </Heading>
-                    </Flex>
-                  )}
-                </Flex>
-              </Box>
-            </Flex>
-          </Stack>
+                )}
+              </Flex>
+            </Box>
+          </Flex>
         ) : (
           <Flex display={"flex"} align={"center"} justify={"center"} h={"90vh"}>
             <Ring speed={2} lineWeight={5} color="blue" size={64} />
