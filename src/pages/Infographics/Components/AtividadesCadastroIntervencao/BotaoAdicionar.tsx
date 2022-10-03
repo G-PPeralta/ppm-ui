3d6cc1afccf90a42452c143d74824dde2057baeb
Eduardo Muchak <eduardomuchak@gmail.com>
2022-10-03 09:34:08 -0300

    fix: :art: ajuste logica habilitar botao adiciona atividade
    modais cadastro intervencao e projeto tipo

--- src/pages/Infographics/Components/AtividadesCadastroIntervencao/BotaoAdicionar.tsx
+++ src/pages/Infographics/Components/AtividadesCadastroIntervencao/BotaoAdicionar.tsx
@@ -13,18 +13,11 @@ function BotaoAdicionar({ add, registerForm }: Props) {
   const [isDisabled, setIsDisabled] = useState(true);
 
   useEffect(() => {
-    if (
-      registerForm.values.atividades[registerForm.values.atividades.length - 1]
-        .tarefa_id === 0
-    ) {
-      setIsDisabled(true);
-    } else {
-      setIsDisabled(false);
-    }
-  }, [
-    registerForm.values.atividades[registerForm.values.atividades.length - 1]
-      .tarefa_id,
-  ]);
+    const isDisabled = registerForm.values.atividades.some(
+      (atividade: any) => atividade.tarefa_id <= 0
+    );
+    setIsDisabled(isDisabled);
+  }, [registerForm.values.atividades]);
 
   return (
     <Flex
