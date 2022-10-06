e12269892dd6dd0db94da305e339a9dfeed76733
max963 <mg_morais@yahoo.com.br>
2022-10-05 22:51:24 -0300

    fix: correções e ajustes na tela de projetos

--- src/pages/Projects/Components/TabelaProjetos.tsx
+++ src/pages/Projects/Components/TabelaProjetos.tsx
@@ -24,6 +24,7 @@ import {
   Select,
 } from "@chakra-ui/react";
 import { Projetos } from "interfaces/Projetos";
+
 import { formatDate } from "utils/formatDate";
 import { formatReal } from "utils/formatReal";
 
