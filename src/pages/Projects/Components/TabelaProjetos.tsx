5df21060b935402246727a17f5fe886bfba5fa3d
Geovana <geovanaag@hotmail.com>
2022-10-08 17:36:32 -0300

    fix: modais tela de projetos

--- src/pages/Projects/Components/TabelaProjetos.tsx
+++ src/pages/Projects/Components/TabelaProjetos.tsx
@@ -54,6 +54,8 @@ export function TabelaProjetos({ data }: TableProps) {
     0
   );
 
+  // console.log({ data });
+
   const tableData = data.slice(from, to).map((projeto, key) => (
     <Tr key={key}>
       <Td isNumeric textAlign={"center"}>
