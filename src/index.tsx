4d8150dab2d9262d2e855cc5501485589046d470
Eduardo Muchak <eduardomuchak@gmail.com>
2022-10-08 18:34:39 -0300

    feat: :sparkles: ajusta mensagem gantt

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,25 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 
 import { ChakraProvider } from "@chakra-ui/react";
-import { registerLicense } from "@syncfusion/ej2-base";
+import {
+  L10n,
+  registerLicense,
+  setCulture,
+  setCurrencyCode,
+} from "@syncfusion/ej2-base";
 import theme from "theme/index";
 
 import { App } from "./App";
+
+L10n.load({
+  "pt-BR": {
+    grid: {
+      EmptyRecord: "Nenhum cronograma encontrado",
+    },
+  },
+});
+setCulture("pt-BR");
+setCurrencyCode("BRL");
 
 const container = document.getElementById("root");
 if (!container) throw new Error("Failed to find the root element");
