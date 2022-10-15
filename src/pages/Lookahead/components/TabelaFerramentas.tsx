00ccaf8fe860cb3aab441ee3b43e1f199e05bab8
max963 <mg_morais@yahoo.com.br>
2022-10-14 23:37:50 -0300

    fix: carregar dados automaticamente na tela inicial do Lookahead

--- src/pages/Lookahead/components/TabelaFerramentas.tsx
+++ src/pages/Lookahead/components/TabelaFerramentas.tsx
@@ -12,6 +12,7 @@ import {
   Th,
   Thead,
   Tr,
+  Text,
 } from "@chakra-ui/react";
 import { FerramentasAtividade } from "interfaces/lookahead";
 
@@ -87,23 +88,28 @@ export function TabelaFerramentas(props: TableProps) {
         <Table variant="unstyled" size={"sm"}>
           <Thead>
             <Tr backgroundColor={"blue"} color="white">
-              <Th colSpan={5} borderTopLeftRadius="10px">
-                Ferramentas
-              </Th>
-              <Th borderTopRightRadius={"10px"} colSpan={2}>
-                <Button
-                  variant="ghost"
-                  colorScheme="messenger"
-                  color="white"
-                  rightIcon={<FiPrinter />}
-                  _hover={{
-                    background: "white",
-                    transition: "all 0.4s",
-                    color: "rgb(46, 105, 253)",
-                  }}
-                >
-                  Exportar
-                </Button>
+              <Th
+                colSpan={7}
+                borderTopRightRadius={"10px"}
+                borderTopLeftRadius="10px"
+                border="none 0px !important"
+              >
+                <Flex justifyContent="space-between" alignItems="center">
+                  <Text>Ferramentas</Text>
+                  <Button
+                    variant="ghost"
+                    colorScheme="messenger"
+                    color="white"
+                    rightIcon={<FiPrinter />}
+                    _hover={{
+                      background: "white",
+                      transition: "all 0.4s",
+                      color: "rgb(46, 105, 253)",
+                    }}
+                  >
+                    Exportar
+                  </Button>
+                </Flex>
               </Th>
             </Tr>
             <Tr backgroundColor={"rgb(46, 105, 253)"} color="white">
@@ -125,39 +131,22 @@ export function TabelaFerramentas(props: TableProps) {
                     ferr.length > 0 ? ferr.map((x) => x.nome).join(" - ") : "";
                   return <Td>{fNames}</Td>;
                 })}
-              {/* <Td>
-                Chave de fenda <br /> 01:00 - 22/08
-              </Td>
-              <Td></Td>
-              <Td></Td>
-              <Td></Td>
-              <Td></Td>
-              <Td></Td>
-              <Td></Td> */}
             </Tr>
           </Tbody>
           <Tfoot>
             <Tr backgroundColor={"blue"} color="white">
               {dias &&
                 ferramentasData &&
-                dias.map(function (dia) {
-                  return (
-                    <Td>
-                      {
-                        ferramentasData.filter(
-                          (x) => x.dia == dia.data && x.tipo == "f"
-                        ).length
-                      }
-                    </Td>
-                  );
+                dias.map(function (dia, key) {
+                  const qtd = ferramentasData.filter(
+                    (x) => x.dia == dia.data && x.tipo == "f"
+                  ).length;
+                  if (key === 0) {
+                    return <Td borderBottomLeftRadius="10px">{qtd}</Td>;
+                  } else if (key === dias.length - 1) {
+                    return <Td borderBottomRightRadius="10px">{qtd}</Td>;
+                  } else return <Td>{qtd}</Td>;
                 })}
-              {/* <Td>Total 1</Td>
-              <Td>0</Td>
-              <Td>0</Td>
-              <Td>0</Td>
-              <Td>0</Td>
-              <Td>0</Td>
-              <Td>0</Td> */}
             </Tr>
           </Tfoot>
         </Table>
