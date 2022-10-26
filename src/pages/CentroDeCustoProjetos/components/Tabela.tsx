b535c407bff5bb8da87eb5909bcd0a682feac747
Felipe Mateus <eu@felipemateus.com>
2022-10-26 00:59:30 -0300

    fix(add custo): add custo
    Add custo

--- src/pages/CentroDeCustoProjetos/components/Tabela.tsx
+++ src/pages/CentroDeCustoProjetos/components/Tabela.tsx
@@ -163,7 +163,7 @@ function Tabela({ data, refreshState, idProjeto, optionsSelects }: Props) {
           })
         ) : (
           <Tr>
-            <Td textAlign={"center"} fontWeight={"semibold"}>
+            <Td colSpan={8} textAlign={"center"} fontWeight={"semibold"}>
               <Text>Não há dados</Text>
             </Td>
           </Tr>
