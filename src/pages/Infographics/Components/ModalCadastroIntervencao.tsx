c152812b2ea46b39d092a51c0a22ee5bbcf3c51e
Geovana <geovanaag@hotmail.com>
2022-11-01 16:23:17 -0300

    fix: modal Cadastro Intervenção - Figma

--- src/pages/Infographics/Components/ModalCadastroIntervencao.tsx
+++ src/pages/Infographics/Components/ModalCadastroIntervencao.tsx
@@ -297,7 +297,8 @@ function ModalCadastroIntervencao({
             display={"flex"}
             justifyContent={"center"}
             color={"white"}
-            fontSize={"1em"}
+            fontSize={"14px"}
+            fontWeight={"700"}
           >
             Cadastrar Nova Intervenção/Perfuração
           </ModalHeader>
