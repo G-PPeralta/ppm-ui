926e24403ad56f1f9c4fe8c22b6c7adda925921c
Bruno Alcantara <bruno.alcantara@iksolution.com.br>
2022-11-24 09:45:56 -0300

    fix: "inclui nova key no card local visaoPorArea"

--- src/pages/VisaoPorArea/components/CardAct.tsx
+++ src/pages/VisaoPorArea/components/CardAct.tsx
@@ -14,6 +14,7 @@ type Atividade = {
   qtddias: number;
   sonda: string;
   ind_alerta?: number;
+  ind_status?: number;
 };
 
 type Props = {
@@ -38,7 +39,8 @@ function CardACT({ atividade, id, setRefresh, refresh }: Props) {
         Number(atividade.comp_pct),
         Number(atividade.pct_real),
         atividade.finalplanejado,
-        Number(atividade.ind_alerta)
+        Number(atividade.ind_alerta),
+        Number(atividade.ind_status)
       )}
       px={5}
       py={3}
