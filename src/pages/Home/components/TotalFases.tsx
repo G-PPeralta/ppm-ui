2c3580b3586e9d5cdadbc151e034f15945b814b6
GeovanaAugusta <geovanaag@hotmail.com>
2022-12-15 17:06:37 -0300

    fix: bug do mês - Total de Projetos

--- src/pages/Home/components/TotalFases.tsx
+++ src/pages/Home/components/TotalFases.tsx
@@ -33,7 +33,17 @@ export default function TotalFases({ data }: any) {
         barGap={0}
         barCategoryGap={0}
       >
-        <XAxis dataKey="mes" />
+        <XAxis
+          dataKey="mes"
+          interval={0}
+          style={{
+            fontSize: "16px",
+            fontFamily: "Mulish",
+            fontWeight: 400,
+            width: "100%",
+            color: "#2D2926",
+          }}
+        />
         <Tooltip />
         <Bar dataKey="Outros" stackId="b" fill="#FEB144" barSize={20} />
         <Bar dataKey="Cancelado" stackId="b" fill="#FF6663" barSize={20} />
