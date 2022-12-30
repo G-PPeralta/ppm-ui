16944023c1b971d89de5cee980b39c0ee99d44fa
Eduardo Muchak <eduardomuchak@gmail.com>
2022-12-30 16:42:39 -0300

    fix: :bug: ajuste posicionamento botao lixeira card pir

--- src/pages/Infographics/Components/CardPIR.tsx
+++ src/pages/Infographics/Components/CardPIR.tsx
@@ -98,6 +98,34 @@ function CardPIR({ poco, index, refresh }: Props) {
           />
         ) : null}
 
+        <IconButton
+          zIndex={2}
+          position={"absolute"}
+          right={22}
+          bottom={2}
+          icon={<FiTrash2 size={18} />}
+          aria-label="Ir para o cronograma de execução"
+          w={"20px"}
+          size={"sm"}
+          backgroundColor={"transparent"}
+          color={"white"}
+          borderRadius={10}
+          _hover={{
+            backgroundColor: "white",
+            color: validateDate(
+              Number(poco.pct_plan),
+              Number(poco.comp_pct),
+              Number(poco.pct_real),
+              poco.finalplanejado,
+              Number(poco.ind_alerta),
+              Number(poco.ind_status)
+            ),
+          }}
+          onClick={() => {
+            deleteIntervencao(poco.id_campanha, poco.id_poco);
+          }}
+        />
+
         <Flex
           direction={"column"}
           align={"center"}
@@ -120,6 +148,7 @@ function CardPIR({ poco, index, refresh }: Props) {
           minW={"220px"}
           position={"relative"}
           onClick={() => transfer()}
+          pb={12}
         >
           <Flex>
             <Text
@@ -214,31 +243,6 @@ function CardPIR({ poco, index, refresh }: Props) {
               >
                 {`${poco.pct_real}%`}
               </Text>
-            </Flex>
-            <Flex justify={"center"} w={"100%"} mt={2}>
-              <IconButton
-                icon={<FiTrash2 size={18} />}
-                aria-label="Ir para o cronograma de execução"
-                w={"20px"}
-                size={"sm"}
-                backgroundColor={"transparent"}
-                color={"white"}
-                borderRadius={10}
-                _hover={{
-                  backgroundColor: "white",
-                  color: validateDate(
-                    Number(poco.pct_plan),
-                    Number(poco.comp_pct),
-                    Number(poco.pct_real),
-                    poco.finalplanejado,
-                    Number(poco.ind_alerta),
-                    Number(poco.ind_status)
-                  ),
-                }}
-                onClick={() => {
-                  deleteIntervencao(poco.id_campanha, poco.id_poco);
-                }}
-              />
             </Flex>
           </Flex>
         </Flex>
--- src/theme/foundations/spacing.ts
+++ src/theme/foundations/spacing.ts
@@ -13,6 +13,7 @@ const spacing = {
   14: "3.5rem",
   16: "4rem",
   20: "5rem",
+  22: "5.5rem",
   24: "6rem",
   28: "7rem",
   32: "8rem",
