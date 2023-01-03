02de9844b4fdb060a12f6628427a7151278ffe5a
Geovana Augusta <geovanaag@hotmail.com>
2023-01-03 15:06:19 -0300

    feat: chave ind_sistema dinâmica - cadastro de outro responsável

--- src/pages/CadastrarProjeto/index.tsx
+++ src/pages/CadastrarProjeto/index.tsx
@@ -112,6 +112,7 @@ function CadastrarProjeto() {
                         propName={"responsavelId"}
                         rota={"/responsavel"}
                         respOuCoord={true}
+                        ind_sistema={"p"}
                       />
                     ) : (
                       <SelectFiltragem
