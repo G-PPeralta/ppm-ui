7cb517e4527f237a2063dc01a52409a679fbe9ea
Eduardo Muchak <eduardomuchak@gmail.com>
2023-01-10 10:57:40 -0300

    feat: :sparkles: adiciona tratamento erro pagina campanhas

--- src/pages/Infographics/index.tsx
+++ src/pages/Infographics/index.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
 
 import { Box, Flex, Heading } from "@chakra-ui/react";
-import { Ring } from "@uiball/loaders";
 
 import ContainerPagina from "components/ContainerPagina";
+import { Loading } from "components/Loading";
+import RequestError from "components/RequestError";
 import Sidebar from "components/SideBar";
 import StatusProjeto from "components/StatusProjeto";
 import TituloPagina from "components/TituloPagina";
@@ -24,6 +25,7 @@ import ModalReorderSimples from "./Components/ModalReorderSimples";
 
 export function Infographics() {
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
   const [campanhas, setCampanhas] = useState<any[]>([]);
   const [refresh, setRefresh] = useState(false);
 
@@ -46,9 +48,13 @@ export function Infographics() {
   };
 
   const handleGetAll = async () => {
-    const campanhas = await postGetInfoCampanha(registerForm.values);
-    setCampanhas(campanhas.data);
-    setLoading(false);
+    try {
+      const campanhas = await postGetInfoCampanha(registerForm.values);
+      setCampanhas(campanhas.data);
+      setLoading(false);
+    } catch (error) {
+      setError(true);
+    }
   };
 
   useEffect(() => {
@@ -61,103 +67,111 @@ export function Infographics() {
     }, 1000);
   }, [refresh]);
 
+  if (loading) {
+    return (
+      <Sidebar>
+        <ContainerPagina>
+          <Loading />
+        </ContainerPagina>
+      </Sidebar>
+    );
+  }
+
+  if (error) {
+    return (
+      <Sidebar>
+        <ContainerPagina>
+          <RequestError />
+        </ContainerPagina>
+      </Sidebar>
+    );
+  }
+
   return (
     <>
       <Sidebar>
-        {!loading ? (
-          <ContainerPagina>
-            <TituloPagina>Acompanhamento de Poços</TituloPagina>
-            <Flex
-              direction={"column"}
-              justify={"space-between"}
-              gap={4}
-              wrap={"wrap"}
-              mb={2}
-              flex={1}
-            >
-              <Flex gap={2} wrap={"wrap"} flex={1} justify={"space-between"}>
-                <Flex gap={2} wrap={"wrap"}>
-                  <ModalCadastrarSonda
-                    refresh={refresh}
-                    setRefresh={setRefresh}
-                  />
-                  <ModalCadastroPoco
-                    refresh={refresh}
-                    setRefresh={setRefresh}
-                  />
-                  <ModalCadastroAtividade />
-                  <ModalCadastroProjetoTipo
-                    refresh={refresh}
-                    setRefresh={setRefresh}
-                  />
-                  <ModalNovaCampanha
-                    refresh={refresh}
-                    setRefresh={setRefresh}
-                  />
-                </Flex>
-                <Flex gap={2}>
-                  <ModalReorderSimples
-                    refresh={refresh}
-                    setRefresh={setRefresh}
-                  />
-                  <FiltrosModal
-                    refresh={refresh}
-                    setRefresh={setRefresh}
-                    listas={listas}
-                    registerForm={registerForm}
-                  />
-                </Flex>
+        <ContainerPagina>
+          <TituloPagina>Acompanhamento de Poços</TituloPagina>
+          <Flex
+            direction={"column"}
+            justify={"space-between"}
+            gap={4}
+            wrap={"wrap"}
+            mb={2}
+            flex={1}
+          >
+            <Flex gap={2} wrap={"wrap"} flex={1} justify={"space-between"}>
+              <Flex gap={2} wrap={"wrap"}>
+                <ModalCadastrarSonda
+                  refresh={refresh}
+                  setRefresh={setRefresh}
+                />
+                <ModalCadastroPoco refresh={refresh} setRefresh={setRefresh} />
+                <ModalCadastroAtividade />
+                <ModalCadastroProjetoTipo
+                  refresh={refresh}
+                  setRefresh={setRefresh}
+                />
+                <ModalNovaCampanha refresh={refresh} setRefresh={setRefresh} />
               </Flex>
-              <Flex gap={4} wrap={"wrap"} flex={1} justify={"end"}>
-                {statusProjeto.map((status, index) => (
-                  <StatusProjeto
-                    key={index}
-                    status={status.status}
-                    color={status.color}
-                  />
-                ))}
+              <Flex gap={2}>
+                <ModalReorderSimples
+                  refresh={refresh}
+                  setRefresh={setRefresh}
+                />
+                <FiltrosModal
+                  refresh={refresh}
+                  setRefresh={setRefresh}
+                  listas={listas}
+                  registerForm={registerForm}
+                />
               </Flex>
             </Flex>
-            <Flex align={"center"} justify={"center"}>
-              {campanhas.length !== 0 ? (
-                <Box
-                  overflowX={{ base: "scroll" }}
-                  display={"flex"}
-                  flexDirection={"row"}
-                  gap={10}
-                  py={2}
-                  flex={1}
-                >
-                  {campanhas.map((column, index) => (
-                    <Flex
-                      key={index}
-                      direction={"column"}
-                      gap={4}
-                      align={"end"}
-                      justify={"space-between"}
-                    >
-                      <ColumnSPT
-                        column={column}
-                        refresh={refresh}
-                        setRefresh={setRefresh}
-                      />
-                    </Flex>
-                  ))}
-                </Box>
-              ) : (
-                <Flex h={180} align={"center"} justify={"center"}>
-                  <Heading as="h4" size="md">
-                    Nenhuma campanha Cadastrada
-                  </Heading>
-                </Flex>
-              )}
+            <Flex gap={4} wrap={"wrap"} flex={1} justify={"end"}>
+              {statusProjeto.map((status, index) => (
+                <StatusProjeto
+                  key={index}
+                  status={status.status}
+                  color={status.color}
+                />
+              ))}
             </Flex>
-          </ContainerPagina>
-        ) : (
-          <Flex display={"flex"} align={"center"} justify={"center"} h={"90vh"}>
-            <Ring speed={2} lineWeight={5} color="blue" size={64} />
           </Flex>
-        )}
+          <Flex align={"center"} justify={"center"}>
+            {campanhas.length !== 0 ? (
+              <Box
+                overflowX={{ base: "scroll" }}
+                display={"flex"}
+                flexDirection={"row"}
+                gap={10}
+                py={2}
+                flex={1}
+              >
+                {campanhas.map((column, index) => (
+                  <Flex
+                    key={index}
+                    direction={"column"}
+                    gap={4}
+                    align={"end"}
+                    justify={"space-between"}
+                  >
+                    <ColumnSPT
+                      column={column}
+                      refresh={refresh}
+                      setRefresh={setRefresh}
+                    />
+                  </Flex>
+                ))}
+              </Box>
+            ) : (
+              <Flex h={180} align={"center"} justify={"center"}>
+                <Heading as="h4" size="md">
+                  Nenhuma campanha Cadastrada
+                </Heading>
+              </Flex>
+            )}
+          </Flex>
+        </ContainerPagina>
       </Sidebar>
     </>
   );
