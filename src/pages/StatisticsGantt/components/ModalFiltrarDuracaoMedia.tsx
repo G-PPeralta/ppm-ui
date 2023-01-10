444d5c68259a2c77fe6098b19be397be7b29a4c5
Bruno Alcantara <bruno.alcantara@iksolution.com.br>
2023-01-10 16:03:18 -0300

    fix: "key payload"

--- src/pages/StatisticsGantt/components/ModalFiltrarDuracaoMedia.tsx
+++ src/pages/StatisticsGantt/components/ModalFiltrarDuracaoMedia.tsx
@@ -63,7 +63,7 @@ export function ModalFiltrarDuracaoMedia({
 
   const getFilter = async () => {
     const payload = {
-      IdOperacao: operacaoId,
+      idOperacao: operacaoId,
       pocoId: registerForm.values.pocoId,
       sondaId: registerForm.values.sondaId,
       profundidadeIni: registerForm.values.profundidadeIni,
